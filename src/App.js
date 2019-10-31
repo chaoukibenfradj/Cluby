ddd8767ef4a238c0354c8c86ed0e4f91c28de0e9
Chaouki Ben Fredj <chaoukibenfraj@gmail.com>
2019-10-31 23:28:11 +0100

    Integrated Antd + deploy script to gh-pgs + logo in index for demo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './App.css';
-
+import logo from './assets/img/logo.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-        Cluby is here
-        </p>
+        <img src={logo} className="App-logo" alt="logo"></img>
+        <p className="App-text">Coming soon ...</p>
       </header>
     </div>
   );
