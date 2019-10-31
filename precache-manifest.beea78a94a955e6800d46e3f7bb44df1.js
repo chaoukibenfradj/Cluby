0c122784afe91e5528cd6f83ef3b1e94a5c761bc
Chaouki Ben Fredj <chaoukibenfraj@gmail.com>
2019-10-31 22:42:35 +0100

    Updates

---
+++ precache-manifest.beea78a94a955e6800d46e3f7bb44df1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91719ef7ddc1fbb953d3d4dfb998275d",
+    "url": "/cluby/index.html"
+  },
+  {
+    "revision": "0c1adbce0a58f481ba7b",
+    "url": "/cluby/static/css/main.e6b731c1.chunk.css"
+  },
+  {
+    "revision": "31522619c681a4248429",
+    "url": "/cluby/static/js/2.312cf107.chunk.js"
+  },
+  {
+    "revision": "0c1adbce0a58f481ba7b",
+    "url": "/cluby/static/js/main.24812d34.chunk.js"
+  },
+  {
+    "revision": "8eddb0e2ee8a661741f8",
+    "url": "/cluby/static/js/runtime-main.e949952a.js"
+  },
+  {
+    "revision": "1c1bbb70e34e978c658b25aa2a0985af",
+    "url": "/cluby/static/media/logo.1c1bbb70.png"
+  }
+]);
