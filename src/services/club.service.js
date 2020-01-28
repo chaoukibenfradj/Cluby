a9f24832498dae29fc926051b43e502c4ae3848e
Hana Zouaoui <32751398+HanaZouaoui@users.noreply.github.com>
2020-01-28 14:51:55 +0100

    clubs details

--- src/services/club.service.js
+++ src/services/club.service.js
@@ -4,4 +4,6 @@ import { BASE_URL } from "../const";
 export const getAllClubs = async () => {
     return await axios.get(BASE_URL + '/api/v1/clubs');
 }
-
+export const getClubById = async (id) => {
+    return await axios.get(BASE_URL + 'api/v1/clubs/' + id);
+}
